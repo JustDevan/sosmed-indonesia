5698faff97a144b1dd29107a3ccbded88a0e1f4f
JustDevan <justdevan1st@gmail.com>
2021-08-29 22:01:38 +0700

    persiapan menambahkan ke heroku

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 
+const admin = 'Admin Devan'
+
+const data = []
+
 app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.use(express.urlencoded())
 
 app.get('/', (req, res) => {
 	res.render('index', {
+		layout: 'layouts/layout-utama',
 		title: 'Home',
-		content: {
-			gambar: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-			nama: 'Ini Devan',
-			caption: 'Ini postingan pertama'
-		}
-		
+		content: data
 	})
 })
 
 app.get('/addpost', (req, res) => {
-	res.send('Ini halaman tambah postingan')
+	res.render('addpost', {
+                layout: 'layouts/layout-utama',
+                title: 'Tambah Postingan',
+		kirim: false
+	})
+})
+
+app.post('/addpost', (req, res) => {
+	data.push(req.body)
+	res.render('addpost', {
+		layout: 'layouts/layout-utama',
+		title: 'Tambah Postingan',
+		kirim: true
+	})
 })
 
 app.listen(3000, () => {
