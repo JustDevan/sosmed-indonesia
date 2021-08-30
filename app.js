7c76e1b7cbb87f8ffbadb946997e89646e407675
JustDevan <justdevan1st@gmail.com>
2021-08-30 12:09:22 +0700

    Menambahkan data.json untuk menyimpan data (sementara

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const fs = require('fs')
 const app = express()
 
 const admin = 'Admin Devan'
 
-const data = []
+const fileData = fs.readFileSync('data/data.json', 'utf-8')
+const data = JSON.parse(fileData)
 
 app.set('view engine', 'ejs')
 app.use(expressLayouts)
@@ -28,6 +30,8 @@ app.get('/addpost', (req, res) => {
 
 app.post('/addpost', (req, res) => {
 	data.push(req.body)
+	fs.writeFileSync('data/data.json', JSON.stringify(data))
+
 	res.render('addpost', {
 		layout: 'layouts/layout-utama',
 		title: 'Tambah Postingan',
